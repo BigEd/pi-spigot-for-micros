26c92c98d55cfd32c638b92a507f81d1069df897
David Banks <dave@hoglet.com>
2024-06-12 12:58:30 +0100

    Build: output labels file for use by profiler

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-beebasm -D BELLARD=0  -i spigot-common.6502.asm -v -do spigot-bbp.ssd     -title "SPIG BBP"     -opt 3 |& tee bbp.log
-beebasm -D BELLARD=-1 -i spigot-common.6502.asm -v -do spigot-bellard.ssd -title "SPIG BELLARD" -opt 3 |& tee bellard.log
+beebasm -D BELLARD=0  -dd -labels     bbp.labels -i spigot-common.6502.asm -v -do     spigot-bbp.ssd -title "SPIG BBP"     -opt 3 |& tee bbp.log
+beebasm -D BELLARD=-1 -dd -labels bellard.labels -i spigot-common.6502.asm -v -do spigot-bellard.ssd -title "SPIG BELLARD" -opt 3 |& tee bellard.log
