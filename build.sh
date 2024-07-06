3cb9739f978d62dc296b52589cb67ffb568ceac6
Ed Spittles <ed.spittles@gmail.com>
2024-07-06 18:42:59 +0100

    improve build script portability

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ do
     do
         NAME=${STEM}${BASE}
 
-        beebasm -D BELLARD=${BELLARD} -D BASE=0x${BASE}00 -dd -labels ${BUILD}/${NAME}.labels -i spigot-runner.6502.asm -v -o ${BUILD}/${NAME}.bin |& tee ${BUILD}/${NAME}.log
+        beebasm -D BELLARD=${BELLARD} -D BASE=0x${BASE}00 -dd -labels ${BUILD}/${NAME}.labels -i spigot-runner.6502.asm -v -o ${BUILD}/${NAME}.bin 2>&1 | tee ${BUILD}/${NAME}.log
 
         echo "PUTFILE \"${BUILD}/${NAME}.bin\",\"${NAME}\",&${BASE}00,&${BASE}00" >> ${BUILD}/${MKSSD}
     done
